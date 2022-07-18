48ad19274c5b7a02d8be694b6e1148aa791689e5
vinchi777 <judecalimbas@gmail.com>
2022-07-18 09:35:32 +0800

    add catppuccin config

---
+++ nvim/lua/plugins/catpuccin.lua
@@ -0,0 +1,13 @@
+local colors = require("catppuccin.palettes").get_palette()
+require("catppuccin").setup {
+	custom_highlights = {
+		Comment = { fg = colors.overlay1 },
+		LineNr = { fg = colors.overlay1 },
+		CursorLine = { bg = colors.none },
+		CursorLineNr = { fg = colors.lavender },
+		DiagnosticVirtualTextError = { bg = colors.none },
+		DiagnosticVirtualTextWarn = { bg = colors.none },
+		DiagnosticVirtualTextInfo = { bg = colors.none },
+		DiagnosticVirtualTextHint = { bg = colors.none },
+	}
+}
